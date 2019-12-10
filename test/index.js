ba06209ee6a2964aac11912d98558da9d5e9b87c
Coen Hyde <coen.hyde@gmail.com>
2019-12-10 01:02:58 -0600

    add test to load nested directory

--- test/index.js
+++ test/index.js
@@ -1,17 +1,22 @@
-var expect = require('expect.js');
-var requirize = require('../');
+const expect = require('expect.js');
+const requirize = require('../');
 
 describe('Requirize', function() {
   it('should require a directory and construct hash', function() {
-    var object = requirize(__dirname+'/fixtures');
+    let object = requirize(__dirname+'/fixtures');
     expect(object['file1']).to.equal('file1');
     expect(object['file.2']).to.equal('file2');
     expect(object['testCamelize']).to.equal('testCamelize');
     expect(object['index']).to.equal(undefined);
   });
 
+  it('should require a nested dir', function() {
+    let object = requirize(__dirname+'/fixtures');
+    expect(object['dir']).to.eql({ file3: 'file3' });
+  });
+
   it('should require a directory and construct hash in classify mode', function() {
-    var object = requirize(__dirname+'/fixtures', 'classify');
+    let object = requirize(__dirname+'/fixtures', 'classify');
     expect(object['File1']).to.equal('file1');
     expect(object['File2']).to.equal('file2');
     expect(object['TestCamelize']).to.equal('testCamelize');
